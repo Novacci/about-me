5f7a6b7ac67f236dfb9da6e53271d8e10315df60
Wojciech Nowak <wojtekn00@gmail.com>
2023-04-28 18:05:01 +0200

    component devide

---
+++ src/components/GitHub.tsx
@@ -0,0 +1,37 @@
+import styles from './GitHub.module.scss';
+import { GithubIcon } from '../Icons/GithubIcon';
+import { RxArrowTopRight } from 'react-icons/rx';
+
+const Github = () => {
+  return (
+    <>
+      <div className={styles['git-container']}>
+        <div className={styles['buttons-grouping']}>
+          <a rel="noreferrer" target="_blank" href="https://github.com/Novacci">
+            <GithubIcon />
+          </a>
+          <a
+            className={styles['git-arrow']}
+            rel="noreferrer"
+            target="_blank"
+            href="https://github.com/Novacci"
+          >
+            <div>
+              <RxArrowTopRight />
+            </div>
+            <span className={styles.text}>Github.com</span>
+          </a>
+        </div>
+        <div>
+          <div className={styles.title}>GitHub</div>
+          <span className={styles.darkened}>
+            Click the button above to see my profile and check out my github
+            projects.
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Github;

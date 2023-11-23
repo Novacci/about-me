3f6e4c73c5b15bdc3b2141ccd48538dd402bbdb9
Wojciech Nowak <wojtekn00@gmail.com>
2023-11-23 20:19:22 +0100

    Created SpotifySpinner component with styling

---
+++ src/components/SpotifySpinner/SpotifySpinner.tsx
@@ -0,0 +1,14 @@
+import styles from './SpotifySpinner.module.scss';
+import { SpotifyIcon } from '../../Icons/SpotifyIcon';
+
+const SpotifySpinner = () => {
+  return (
+    <>
+      <div className={styles[`spotify-logo`]}>
+        <SpotifyIcon />
+      </div>
+    </>
+  );
+};
+
+export default SpotifySpinner;
